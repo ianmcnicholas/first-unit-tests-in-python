563934969b94509e83e6fa90eb665332f943f8fd
ianmcnicholas <ianjmcnicholas@.com>
2024-02-21 17:05:01 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learning the Basics of Unit Testing in Python
+
+A simple project putting together some unit tests using pytest.  Also demonstrating TDD and learning how to interact with tests in the VS Code shell.
---
+++ test_pytest.py
@@ -0,0 +1,10 @@
+import inc_dec
+
+def test_increment():
+    assert inc_dec.increment(3) == 4
+
+def test_decrement():
+    assert inc_dec.decrement(3) == 2
+
+def test_incrementByTwo():
+    assert inc_dec.incrementByTwo(3) == 5
